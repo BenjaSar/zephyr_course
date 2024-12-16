bd81200bec2b6e12678eb36427c7026a9a966dff
BenjaSar <fsarmiento1805@gmail.com>
2024-12-16 09:28:25 -0300

    The home task 2 was updated - Manage different Zephyr versions

---
+++ versions/app/build/zephyr/zephyr.dts.d
@@ -0,0 +1,11 @@
+empty_file.o: \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/misc/empty_file.c \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/boards/posix/native_posix/native_posix_64.dts \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/boards/posix/native_posix/native_posix.dts \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/boards/posix/native_posix/../native_sim/native_sim.dts \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/dts/posix/posix.dtsi \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/dts/common/skeleton.dtsi \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/dts/common/mem.h \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/fsiof/course_zephyr/v3_6_0/zephyr_3_6/zephyr/include/zephyr/dt-bindings/gpio/gpio.h
